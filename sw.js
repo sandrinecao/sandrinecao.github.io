469168f9b335b0a3f4149da76a925a9cbd9c9e18
Sandrine Cao <sandrine.cao@gmail.com>
2020-06-12 10:14:24 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-46f9e320c719986ff7a3.js"
+    "url": "webpack-runtime-dd5c26004a46ee01b410.js"
   },
   {
     "url": "framework-43898561dafbdf32aefd.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "00c30f3ff4284251edfa406ae3c3ccd5"
+    "revision": "0dbfce838bb173eb6eadc1e4fc19e661"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-2a242c05cade058abd28.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "9d089a5b6c01ddb3c6bf398496705b77"
+    "revision": "2de50c9864739133497dee368c502056"
   },
   {
     "url": "manifest.webmanifest",

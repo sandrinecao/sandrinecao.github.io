8568b213b7ff14e7705bf8b339764945cd25b0d1
Sandrine Cao <sandrine.cao@gmail.com>
2020-06-19 12:11:32 +0200

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f2cc401d9cb3bb3701daf0f9b1f0c8e4"
+    "revision": "c66c2a365a4c8afeab6e327674ffac3c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-2a242c05cade058abd28.js"
